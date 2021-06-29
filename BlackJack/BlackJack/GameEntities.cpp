5c1b1b02087357d1b31478ec81691fa7538d5ca5
fanglores <kostiktsat@gmail.com>
2021-06-29 15:08:24 +0300

    Created templates

---
+++ BlackJack/BlackJack/GameEntities.cpp
@@ -0,0 +1,25 @@
+class Menu
+{
+
+
+
+
+};
+
+
+class Deck
+{
+private:
+
+public:
+
+};
+
+class Player
+{
+private:
+
+public:
+
+
+};
---
+++ BlackJack/BlackJack/Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include "GameEntities.cpp"
+using namespace std;
+
+int main()
+{
+	
+
+}
