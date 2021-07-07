9e1890ed6e2d9b8928d10f2cc7d511f18f0c0c66
fanglores <kostiktsat@gmail.com>
2021-07-07 13:36:07 +0300

    Card shuffle test

--- BlackJack/BlackJack/Source.cpp
+++ BlackJack/BlackJack/Source.cpp
@@ -4,6 +4,26 @@ int main()
 {
 	setlocale(LC_ALL, "rus");
 
+	vector<long long unsigned int> vect(9, 0);
+	Deck dk;
+
+	long long unsigned int i = 0, cnt = 0;
+
+	while (true)
+	{
+		cnt++;
+		vect[dk.getCard()]++;
+
+		if (i == 100000)
+		{
+			for (long long unsigned int j : vect) cout << (double)j / cnt << " ";
+			cout << endl;
+		}
+
+		i++;
+	}
+
+	/*
 	Menu menu(100, 300);
 
 	int code = menu.MainMenu();
@@ -14,15 +34,13 @@ int main()
 		Deck deck;
 		menu.GameMenu(player_array, &deck);
 	}
-
+	*/
 	return 0;
 }
 
 
 /*
 To do:
-fix: система подсчета - когда у дилера перебор, игрок проигрывает
-
 имена игрокам?
 выход в главное меню?
 унифицировать шрифт?
