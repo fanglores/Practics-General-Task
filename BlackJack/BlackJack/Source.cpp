0a3f6c675f2ed3948133a95c35481331efdf7728
fanglores <kostiktsat@gmail.com>
2021-07-08 10:32:44 +0300

    Input & Deck tests

--- BlackJack/BlackJack/Source.cpp
+++ BlackJack/BlackJack/Source.cpp
@@ -3,7 +3,45 @@
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	
+	Menu mn(100, 300);
+
+	cout << string(100, '=') << "\t\t\tTest 1" << endl << string(100, '=') << endl;
+
+	//Test 1
+
+	cout << "Ввод символа: ";
+	cout << "Функция ввода вернула: " << mn.secure_cin() << endl << endl;
+
+	cout << "Ввод строки: ";
+	cout << "Функция ввода вернула: " << mn.secure_cin() << endl << endl;
+
+	cout << "Ввод строки 2: ";
+	cout << "Функция ввода вернула: " << mn.secure_cin() << endl << endl;
+
+	cout << "Ввод числа: ";
+	cout << "Функция ввода вернула: " << mn.secure_cin() << endl << endl;
+
+	cout << "Ввод большого числа: ";
+	cout << "Функция ввода вернула: " << mn.secure_cin() << endl << endl;
+
+	cout << "Ввод отрицательного числа 1: ";
+	cout << "Функция ввода вернула: " << mn.secure_cin() << endl << endl;
+
+	cout << "Ввод отрицательного числа 2: ";
+	cout << "Функция ввода вернула: " << mn.secure_cin() << endl << endl;
+
+	cout << "Ввод корректного числа 1: ";
+	cout << "Функция ввода вернула: " << mn.secure_cin() << endl << endl;
+
+	cout << "Ввод корректного числа 1: ";
+	cout << "Функция ввода вернула: " << mn.secure_cin() << endl << endl;
+
+	cout << "Ввод корректного числа 1: ";
+	cout << "Функция ввода вернула: " << mn.secure_cin() << endl << endl;
+
+	cout << endl << string(100, '=') << "\t\t\tTest 2" << endl << string(100, '=') << endl;
+
+	//Test 2
 	Deck dk;
 
 	cout << "Начальная колода: ";
@@ -20,6 +58,7 @@ int main()
 	for (int i = 0; i < 36; i++)
 		cout << dk.decodeCard(dk.getCard());
 
+	cout << endl << endl << endl;
 
 	/*
 	Menu menu(100, 300);
