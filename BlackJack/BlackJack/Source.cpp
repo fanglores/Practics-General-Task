91c3c1ffd25c42eea75c1d2158977989602501bf
fanglores <kostiktsat@gmail.com>
2021-07-08 10:07:37 +0300

    Deck test

--- BlackJack/BlackJack/Source.cpp
+++ BlackJack/BlackJack/Source.cpp
@@ -3,7 +3,24 @@
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	
+
+	Deck dk;
+	cout << endl << "Начальная колода: ";
+	for (int i = 0; i < 36; i++)
+		cout << dk.decodeCard(dk.getCard());
+
+	dk.ShuffleDeck();
+	cout << endl << "Начальная колода: ";
+	for (int i = 0; i < 36; i++)
+		cout << dk.decodeCard(dk.getCard());
+
+	dk.DeckFlush();
+	cout << endl << "Начальная колода: ";
+	for (int i = 0; i < 36; i++)
+		cout << dk.decodeCard(dk.getCard());
+
+
+	/*
 	Menu menu(100, 300);
 
 	int code = menu.MainMenu();
@@ -14,7 +31,7 @@ int main()
 		Deck deck;
 		menu.GameMenu(player_array, &deck);
 	}
-	
+	*/
 	return 0;
 }
 
